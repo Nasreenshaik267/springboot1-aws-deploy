23aa79488a0d7a9f19f689873a533e43a65db097
Nasreen <you@example.com>
2024-09-11 17:38:50 -0400

    first commit

---
+++ src/main/java/com/example/spring1_aws_deploy/Spring1AwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring1_aws_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1AwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1AwsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring1_aws_deploy/Spring1AwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring1_aws_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1AwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
